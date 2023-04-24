9c6b06e32fde3f280508669fbf37873d0654ddd3
Anton Balabanov <asbbnv@gmail.com>
2023-04-25 01:51:04 +0300

    run configurations

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,6 @@
 const config = {
-    // API URL to get sensor data. For prod the port should coincide with frontend's port,
-    // as it will be proxied by nginx anyway. For test the port should be Django dev server's port
-    // apiUrl: 'http://' + process.env.FRONTEND_HOST + ':' + process.env.FRONTEND_PORT + '/api/sensors/',
-    apiUrl: 'http://localhost:8000/api/sensors/',
+    // API URL to get sensor data
+    apiUrl: 'http://django:8000/api/sensors/',
 
 
     // For default selected metrics specification of 'All' takes precedence, then 'None', then any specific metric
